eacdf9e89052c6cc80d1138f393b80f90c7631c9
Ayush Shashank <ayush.bibhas@gmail.com>
2023-04-30 16:49:17 +0530

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('get_hello')
+  async getHello(data: Record<string, unknown>): Promise<any> {
+    console.log('event received')
+    return this.appService.getHello(data);
+  }
+  @MessagePattern({ cmd: 'get_hello_msg' })
+  async getHelloMsg(data: Record<string, unknown>): Promise<any> {
+    return this.appService.getHello(data);
   }
 }
