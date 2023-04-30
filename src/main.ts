46eadf45244a717187fe5cf9304bce36ad13e9bd
Ayush Shashank <ayush.bibhas@gmail.com>
2023-04-30 16:48:38 +0530

    microservice and config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { GlobalExceptionFilter } from './filters/GlobalExceptionFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+  const host = config.get<string>('USER_HOST', 'localhost');
+  const port = +config.get<number>('USER_PORT', 3001);
+  const userService = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: port,
+      },
+    },
+  );
+  userService.useGlobalFilters(new GlobalExceptionFilter());
+  await userService.listen();
 }
 bootstrap();
