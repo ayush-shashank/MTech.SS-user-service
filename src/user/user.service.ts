a2a2219850b81295a0a0a6914be74b50a236800f
Ayush Shashank <ayush.shashank@accenture.com>
2023-05-01 19:15:49 +0530

    user changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { Observable, of, throwError } from 'rxjs';
-import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
   create(createUserDto: CreateUserDto) {
@@ -26,15 +23,17 @@ export class UserService {
 
   findOne(id: number) {
     return this.usersRepository.findOneBy({ id });
-    return `This action returns a #${id} user`;
+    // return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.usersRepository.update(id, updateUserDto);
+    // return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.usersRepository.delete(id);
+    // return `This action removes a #${id} user`;
   }
 
   login(credentials: { username: string; password: string }) {
